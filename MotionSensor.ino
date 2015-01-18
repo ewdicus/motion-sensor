1c4ef1fd4f54cde58fe59c7be20311502daeb008
Elliott <ewdicus@gmail.com>
2015-01-18 12:15:52 -0800

    Remove commented code. Use built in led to debug motion.

--- MotionSensor.ino
+++ MotionSensor.ino
@@ -1,16 +1,25 @@
 #define FULL_LIGHT_DELAY 2000 // ms
 
 const int pirPin = 0;
-const int ledPin = 1;
+const int builtInLed = 1;
+const int ledPin = 4;
 
 int pwmValue = 0;
 float pwmCounter = 0.0;
 
 void setup(){
- Serial.begin(9600);
  pinMode(pirPin, INPUT);
  pinMode(ledPin, OUTPUT);
+ pinMode(builtInLed, OUTPUT);
  digitalWrite(ledPin, LOW);
+
+ digitalWrite(builtInLed, HIGH);
+ delay(100);
+ digitalWrite(builtInLed, LOW);
+ delay(100);
+ digitalWrite(builtInLed, HIGH);
+ delay(100);
+ digitalWrite(builtInLed, LOW);
 }
 
 
@@ -18,49 +27,38 @@ void loop(){
 
   // Have we seen motion?
   if(movementDetected()){
-    // Increase PWM value until we're at 255
-    while(pwmValue < 255){
-        pwmValue += 1;
-        analogWrite(ledPin, pwmValue);
-        delay(1);
+    digitalWrite(builtInLed, HIGH);
+
+    // Increase PWM value until we're at 255 - Also check
+    while(pwmCounter < 1.57){
+
+      // pwmValue += 1;
+      pwmValue = sin(pwmCounter) * 255.0;
+      pwmCounter += 0.01;
+
+      analogWrite(ledPin, pwmValue);
+      delay(10);
     }
 
     // Once we're at 255, stay on for the FULL_LIGHT_DELAY
     delay(FULL_LIGHT_DELAY);
 
   }else{
+    digitalWrite(builtInLed, LOW);
+
     // Decrease PWM value until we're at 0
-    if(pwmValue > 0){
-        pwmValue -= 1;
-        analogWrite(ledPin, pwmValue);
-        delay(1);
+    if(pwmCounter > 0){
+
+      if(pwmCounter < 0) pwmCounter = 0;
+
+      // pwmValue -= 1;
+      pwmValue = sin(pwmCounter) * 255.0;
+      pwmCounter -= 0.01;
+
+      analogWrite(ledPin, pwmValue);
+      delay(10);
     }
   }
-
-  // if(pirVal == LOW){ // Yes - Fade to high
-  //   digitalWrite(ledPin, HIGH);
-  //   // while(pwmValue < 255){
-
-  //   //   pwmValue = sin(pwmCounter) * 255;
-  //   //   pwmCounter += 0.01;
-
-  //   //   analogWrite(ledPin, pwmValue);
-  //   //   delay(1);
-  //   // }
-
-  //   // Stay on for a bit
-  //   delay(2000);
-
-  // }else{ // No
-  //   digitalWrite(ledPin, LOW);
-  //   // If we're on fade to low
-  //   // if(pwmValue > 0){
-  //   //   pwmValue = sin(pwmCounter) * 255;
-  //   //   pwmCounter -= 0.01;
-  //   //   analogWrite(ledPin, pwmValue);
-  //   //   delay(1);
-  //   // }
-  // }
 }
 
 bool movementDetected(){
