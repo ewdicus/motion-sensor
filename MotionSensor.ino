3a58d54fb72cdb800cff2a8095b6d035f9f098ac
Elliott <ewdicus@gmail.com>
2015-01-18 11:17:26 -0800

    Simple linear fading

--- MotionSensor.ino
+++ MotionSensor.ino
@@ -1,5 +1,10 @@
+#define FULL_LIGHT_DELAY 2000 // ms
+
 const int pirPin = 0;
 const int ledPin = 1;
+
+int pwmValue = 0;
+float pwmCounter = 0.0;
 
 void setup(){
  Serial.begin(9600);
@@ -10,13 +15,57 @@ void setup(){
 
 
 void loop(){
-  int pirVal = digitalRead(pirPin);
 
-  if(pirVal == LOW){ //was motion detected
-    digitalWrite(ledPin, HIGH);
+  // Have we seen motion?
+  if(movementDetected()){
+    // Increase PWM value until we're at 255
+    while(pwmValue < 255){
+        pwmValue += 1;
+        analogWrite(ledPin, pwmValue);
+        delay(1);
+    }
+
+    // Once we're at 255, stay on for the FULL_LIGHT_DELAY
+    delay(FULL_LIGHT_DELAY);
+
   }else{
-    digitalWrite(ledPin, LOW);
+    // Decrease PWM value until we're at 0
+    if(pwmValue > 0){
+        pwmValue -= 1;
+        analogWrite(ledPin, pwmValue);
+        delay(1);
+    }
   }
-  delay(100);
 
+  // if(pirVal == LOW){ // Yes - Fade to high
+  //   digitalWrite(ledPin, HIGH);
+  //   // while(pwmValue < 255){
+
+  //   //   pwmValue = sin(pwmCounter) * 255;
+  //   //   pwmCounter += 0.01;
+
+  //   //   analogWrite(ledPin, pwmValue);
+  //   //   delay(1);
+  //   // }
+
+  //   // Stay on for a bit
+  //   delay(2000);
+
+  // }else{ // No
+  //   digitalWrite(ledPin, LOW);
+  //   // If we're on fade to low
+  //   // if(pwmValue > 0){
+  //   //   pwmValue = sin(pwmCounter) * 255;
+  //   //   pwmCounter -= 0.01;
+  //   //   analogWrite(ledPin, pwmValue);
+  //   //   delay(1);
+  //   // }
+  // }
+}
+
+bool movementDetected(){
+    if(digitalRead(pirPin) == LOW){
+        return true;
+    }
+    return false;
 }
